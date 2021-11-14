b39113368b6834a83af7c452bb5dae48323a10b0
nabilram <89229716+nabilram@users.noreply.github.com>
2021-11-14 01:08:41 +0100

    10.6.1

--- scraping.py
+++ scraping.py
@@ -1,13 +1,32 @@
 # Import Dependencies
-# Splinter, BeautifulSoup, Pandas
+# Splinter, BeautifulSoup, Pandas, DateTime
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+
 
 # Setup Splinter Browser
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+
+def scrape_all():
+    # Initiate headless(?) driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    data = {
+      "news_title": news_title,
+      "news_paragraph": news_paragraph,
+      "featured_image": featured_image(browser),
+      "facts": mars_facts(),
+      "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 #### 1 - SCRAPE MARS NEWS ARTICLE
 def mars_news(browser):
@@ -26,7 +45,7 @@ def mars_news(browser):
     # Adding Try and Except for Error handling
     try:
         slide_elem = news_soup.select_one('div.list_text')
-        slide_elem.find('div', class_='content_title')
+        #slide_elem.find('div', class_='content_title')
 
         # Use the parent element to find the first `a` tag (for article title) and save it as `news_title`
         news_title = slide_elem.find('div', class_='content_title').get_text()
@@ -35,6 +54,7 @@ def mars_news(browser):
         # Use the parent element to find the paragraph text - article teaser
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
         #news_p
+
     except AttributeError:
         return None, None
     
@@ -58,13 +78,12 @@ def featured_image(browser):
     try:
         # Find the relative image url
         img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-        #img_url_rel
 
-        # Use the base URL to create an absolute URL becuse SRC link is only partial
-        img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-        #img_url
     except AttributeError:
         return None
+
+    # Use the base URL to create an absolute URL becuse SRC scraped URL is only partial
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
     return img_url
 
@@ -74,18 +93,22 @@ def featured_image(browser):
 
 def mars_facts():
     # Add try except for error handling -- Base Exeption since errors
-    # other than Attribute errors may occur (since were scraping a table with pandas)
+    # other than Attribut Errors may occur (since were scraping a table with pandas)
     try:
+        #read_html to convert table, index it to fond the first one
         df = pd.read_html('https://galaxyfacts-mars.com')[0]
+
     except BaseException:
         return None
 
     # Add column names and set 1st column (desc) as index
     df.columns=['description', 'Mars', 'Earth']
     df.set_index('description', inplace=True)
-    #df
+    
 
-#convert pulled df into html + bootstrap
-    return df.to_html()
+    #convert pulled df into html + bootstrap (striped table)
+    return df.to_html(classes="table table-striped")
 
-browser.quit()
+if __name__ == "__main__":
+    # If this is being ran as a script, then print scraped data
+    print(scrape_all())
